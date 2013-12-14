0a4318d408293a4ca3a085d4dbfbcebe5406a730
Josh Phipps <josh.phipps@rapp.com>
2013-12-13 19:33:50 -0600

    completed parameters an annotations on lol api controller

--- src/MetaLeague/APIBundle/Controller/LeagueOfLegendsController.php
+++ src/MetaLeague/APIBundle/Controller/LeagueOfLegendsController.php
@@ -72,46 +72,225 @@ class LeagueOfLegendsController extends FOSRestController
         
     }
     
+    
+    /**
+     * Get a summoner's ID by name.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="name", requirements="*+", nullable=false, description="The Summoner's name.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getSummonerByName(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get recent games by summoner ID.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getSummonerByID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    /**
+     * Get multiple summoner's IDs by csv of names.  Max 40.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="name", requirements="*+", nullable=false, description="CSV of  up to 40 summoner names.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getSummonerNamesByIDs(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoner's masteries by summoner ID.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getMasteriesBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoner's runes by summoner ID.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getRunesBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoners league.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getLeagueBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoner's team.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getTeamBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoners ranked stats by by summoner ID.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getRankedStatsBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
     }
     
+    
+    /**
+     * Get a summoners summary stats by by summoner ID.
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   statusCodes = {
+     *     200 = "Returned when successful"
+     *   }
+     * )
+     *
+     * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     *
+     * @Annotations\View()
+     *
+     * @param Request               $request      the request object
+     * @param ParamFetcherInterface $paramFetcher param fetcher service
+     *
+     * @return array
+     */
     public function getSummaryStatsBySummonerID(Request $request, ParamFetcherInterface $paramFetcher)
     {
         
