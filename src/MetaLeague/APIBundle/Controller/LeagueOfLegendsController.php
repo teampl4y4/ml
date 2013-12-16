1eebaf8ab1c67a62d44e5be3c09d6c8397a0052a
Josh Phipps <josh.phipps@rapp.com>
2013-12-15 23:12:55 -0600

    add region to api…finalize all calls in controller

--- src/MetaLeague/APIBundle/Controller/LeagueOfLegendsController.php
+++ src/MetaLeague/APIBundle/Controller/LeagueOfLegendsController.php
@@ -20,6 +20,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Lsw\ApiCallerBundle\Call\HttpGetJson;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 /**
  * Rest controller for League of Legends
  *
@@ -28,11 +30,27 @@ use Lsw\ApiCallerBundle\Call\HttpGetJson;
  */
 class LeagueOfLegendsController extends FOSRestController
 {
+    
+    private $api_key = null;
+    private $api_params = array();
+    const API_URL = "https://prod.api.pvp.net/api/lol";
+    const API_VERSION = 'v1.1';
+    
+    public function call_api($endpoint, $region = 'na', array $params = null){
+        $this->api_key = $this->container->getParameter('meta_league_api.game.league_of_legends.api.key');
+        $this->api_params['api_key'] = $this->api_key;
+        $url = self::API_URL . "/" . $region . "/" . self::API_VERSION . "/" . $endpoint;
+        $params = $params ? $params : $this->api_params;
+        $output = $this->get('api_caller')->call(new HttpGetJson($url,$params));
+        return new JsonResponse($output);
+    }
+
+
     /**
      * List all champions.
      *
      * 
-     * @Get("/lol/champions")
+     * @Get("/lol/champions/{region}")
      * 
      * @ApiDoc(
      *   resource = true,
@@ -41,18 +59,20 @@ class LeagueOfLegendsController extends FOSRestController
      *   }
      * )
      *
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @return array
      */
-    public function getAllChampions()
+    public function getAllChampions($region)
     {
-        
+        $endpoint = "champion";
+        return $this->call_api($endpoint,$region);  
     }
     
     /**
      * Get recent games by summoner ID.
      * 
-     * @Get("/lol/summoner/games/{summonerID}")
+     * @Get("/lol/summoner/games/{summonerID}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -62,6 +82,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -69,16 +90,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getRecentGamesBySummonerID($summonerID)
+    public function getRecentGamesBySummonerID($summonerID,$region)
     {
-        
+        $endpoint = "game/by-summoner/".$summonerID."/recent";
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoner's ID by name.
      * 
-     * @Get("/lol/summoner/name/{summonerName}")
+     * @Get("/lol/summoner/name/{summonerName}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -87,7 +109,8 @@ class LeagueOfLegendsController extends FOSRestController
      *   }
      * )
      *
-     * @Annotations\QueryParam(name="name", requirements="*+", nullable=false, description="The Summoner's name.")
+     * @Annotations\QueryParam(name="summonerName", requirements="*+", nullable=false, description="The Summoner's name.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      * 
@@ -95,17 +118,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getSummonerByName($summonerName)
+    public function getSummonerByName($summonerName,$region)
     {
-        $output = $this->get('api_caller')->call(new HttpGetJson("https://prod.api.pvp.net/api/lol/na/v1.1/summoner/by-name/".$summonerName,array('api_key'=>"ef24b3c7-5180-4318-9946-319330ea84f4")));
-        return new \Symfony\Component\HttpFoundation\JsonResponse($output);
+        $endpoint = "summoner/by-name/".$summonerName;
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get summoner by ID.
      * 
-     * @Get("/lol/summoner/{summonerID}")
+     * @Get("/lol/summoner/{summonerID}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -115,6 +138,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -122,15 +146,16 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getSummonerByID($summonerID)
+    public function getSummonerByID($summonerID, $region)
     {
-        
+        $endpoint = "summoner/".$summonerID;
+        return $this->call_api($endpoint,$region);
     }
     
     /**
      * Get multiple summoner's IDs by csv of names.  Max 40.
      * 
-     * @Get("/lol/summoner/names/{summonerIDs}")
+     * @Get("/lol/summoner/names/{summonerIDs}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -140,6 +165,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="name", requirements="*+", nullable=false, description="CSV of  up to 40 summoner names.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -147,16 +173,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getSummonerNamesByIDs($summonerIDs)
+    public function getSummonerNamesByIDs($summonerIDs, $region)
     {
-        
+        $endpoint = "summoner/".$summonerIDs."/name";
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoner's masteries by summoner ID.
      * 
-     * @Get("/lol/summoner/masteries/{summonerID}")
+     * @Get("/lol/summoner/masteries/{summonerID}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -166,6 +193,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -173,16 +201,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getMasteriesBySummonerID($summonerID)
+    public function getMasteriesBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "summoner/".$summonerID."/masteries";
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoner's runes by summoner ID.
      * 
-     * @Get("/lol/summoner/runes/{summonerID}")
+     * @Get("/lol/summoner/runes/{summonerID}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -192,6 +221,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -199,16 +229,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getRunesBySummonerID($summonerID)
+    public function getRunesBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "summoner/".$summonerID."/runes";
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoners league.
      * 
-     * @Get("/lol/summoner/league/{summonerID}")
+     * @Get("/lol/summoner/league/{summonerID}/{region}")
      *
      * @ApiDoc(
      *   resource = true,
@@ -218,6 +249,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -225,15 +257,16 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getLeagueBySummonerID($summonerID)
+    public function getLeagueBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "league/by-summoner/".$summonerID;
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoner's team.
-     * @Get("/lol/summoner/team/{summonerID}")
+     * @Get("/lol/summoner/team/{summonerID}/{region}")
      * @ApiDoc(
      *   resource = true,
      *   statusCodes = {
@@ -243,6 +276,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -250,15 +284,16 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getTeamBySummonerID($summonerID)
+    public function getTeamBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "team/by-summoner/".$summonerID;
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoners ranked stats by by summoner ID.
-     * @Get("/lol/summoner/stats/ranked/{summonerID}")
+     * @Get("/lol/summoner/stats/ranked/{summonerID}/{region}")
      *
      * @ApiDoc(
      *   resource = true,
@@ -269,6 +304,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -276,16 +312,17 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getRankedStatsBySummonerID($summonerID)
+    public function getRankedStatsBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "stats/by-summoner/".$summonerID."/ranked";
+        return $this->call_api($endpoint,$region);
     }
     
     
     /**
      * Get a summoners summary stats by by summoner ID.
      * 
-     * @Get("/lol/summoner/stats/summary/{summonerID}")
+     * @Get("/lol/summoner/stats/summary/{summonerID}/{region}")
      *
      * @ApiDoc(
      *   resource = true,
@@ -296,6 +333,7 @@ class LeagueOfLegendsController extends FOSRestController
      * )
      *
      * @Annotations\QueryParam(name="summonerID", requirements="\d+", nullable=false, description="The Summoner ID.")
+     * @Annotations\QueryParam(name="region", requirements="*+", nullable=false, description="The Summoner's region.")
      *
      * @Annotations\View()
      *
@@ -303,8 +341,9 @@ class LeagueOfLegendsController extends FOSRestController
      *
      * @return array
      */
-    public function getSummaryStatsBySummonerID($summonerID)
+    public function getSummaryStatsBySummonerID($summonerID, $region)
     {
-        
+        $endpoint = "stats/by-summoner/".$summonerID."/summary";
+        return $this->call_api($endpoint,$region);  
     }
 }
